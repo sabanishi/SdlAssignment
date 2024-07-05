ac408338aaa02100fb318dee37efa192d174d2b9
sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-07-05 21:15:27 +0900

    改行のルール変更

--- Assets/Scripts/Console/InputView.cs
+++ Assets/Scripts/Console/InputView.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Unit = UniRx.Unit;
 
@@ -17,12 +19,14 @@ namespace Sabanishi.SdiAssignment
         public IObservable<string> OnInputValueChanged => inputField.onValueChanged.AsObservable();
         public IObservable<Unit> OnSendButtonClicked => sendButton.OnClickAsObservable();
 
+        private const int LineLimit = 8;
+
         public void Setup(CancellationToken token)
         {
             OnInputValueChanged.Subscribe(x =>
             {
                 //文字列の行数が制限に達していた時、末尾の改行を無くす
-                if (x.Split("\n").Length > inputField.lineLimit)
+                if (x.Split("\n").Length > LineLimit)
                 {
                     SetText(x.Substring(0, x.Length - 1));
                 }
@@ -31,6 +35,31 @@ namespace Sabanishi.SdiAssignment
 
         public void Cleanup()
         {
+        }
+
+        private void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject == inputField.gameObject)
+            {
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        //改行する
+                        UniTask.Void(async () =>
+                        {
+                            inputField.ActivateInputField();
+                            await UniTask.Yield();
+                            inputField.text += "\n";
+                            inputField.MoveTextEnd(false);
+                        });
+                    }
+                    else
+                    {
+                        sendButton.onClick.Invoke();
+                    }
+                }
+            }
         }
         
         public void SetSendButtonActive(bool isActive)
